1ca63ddf78a97bf73a5379efb0fb48e2907f7cd8
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2024-02-22 15:25:07 -0500

    navBar and mainWindow content

--- portfolio/src/components/navBar/navbar.js
+++ portfolio/src/components/navBar/navbar.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/BasicLogo.png'
+ import messageLogo from '../../assets/messageLogo2.png'
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
-            <img src={logo} alt="" className='logo'/>
+            <img src={logo} alt="Logo" className='logo'/>
             <div className='desktopMenu'>
-
+                <Link className="desktopMenuListItem">Home</Link>
+                <Link className="desktopMenuListItem">Portfolio</Link>
+                <Link className="desktopMenuListItem">About</Link>
+                <Link className="desktopMenuListItem">Clients</Link>
             </div>
             <button className='desktopMenuButton'>
-                <img src="" alt="" className='desktopMenuImage'/>
+                <img src={messageLogo} alt="" className='desktopMenuImage'/>
+                Contact Me
             </button>
         </nav>
     )
