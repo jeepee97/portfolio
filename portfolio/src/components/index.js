626776a92d2b6f539c998bfd0d223acc2cd68de7
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2024-02-23 12:09:49 -0500

    Aboutme basic UI done

--- portfolio/src/components/index.js
+++ portfolio/src/components/index.js
@@ -1,1 +1,2 @@
 export { default as Navbar} from './navBar/navbar'
+export { default as Feature } from './feature/feature'
--- portfolio/src/containers/aboutMe/aboutMe.js
+++ portfolio/src/containers/aboutMe/aboutMe.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './aboutMe.css'
+import { Feature } from '../../components'
 
 const AboutMe = () => {
     return (
         <div className='aboutme section__margin' id='about'>
             <div className='aboutme-feature'>
-                
+                <Feature/>
+            </div>
+            <div className="aboutme-heading">
+                <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+                <p>Explore the library</p>
+            </div>
+            <div className='aboutme-container'>
+                <Feature/>
+                <Feature/>
+                <Feature/>
             </div>
         </div>
     )
