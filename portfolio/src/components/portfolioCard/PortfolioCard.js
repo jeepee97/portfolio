14bf9945043f4ab359cbe354dc1ab8078ee45168
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2024-02-24 13:42:10 -0500

    Completed portfolio card template

--- portfolio/src/components/portfolioCard/PortfolioCard.js
+++ portfolio/src/components/portfolioCard/PortfolioCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './PortfolioCard.css'
+import Feature from '../feature/feature'
 
 
 const PortfolioCard = ({title, text, image}) => {
     return (
         <div className="card">
-            <div>
-                
+            <div className='card-content'>
+                <Feature title={title} text={text}/>
+            </div>
+            <div className='card-image'>
+                <img src={image} alt="image"/>
             </div>
         </div>
     )
--- portfolio/src/pages/Portfolio/portfolio.js
+++ portfolio/src/pages/Portfolio/portfolio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './portfolio.css'
+import WebsiteImage from '../../assets/photo_comité.jpg'
 import { Navbar, PortfolioCard } from '../../components'
 import { CV } from '../../containers'
 
@@ -10,7 +11,7 @@ const Portfolio = () => {
                 <Navbar/>
             </div>
             <div className='portfolio-content section__margin'>
-                <PortfolioCard title="Test title" text="Test text"/>
+                <PortfolioCard title="Website" image={WebsiteImage} text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"/>
             </div>
         </div>
     )
