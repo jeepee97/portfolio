b434c47bb000b18db4e46ae94f358608e32ad955
jeanphilippe anctil <jeanphilippe.anctil@hotmail.com>
2024-02-24 18:44:34 -0500

    fix dependencies

--- portfolio/src/containers/emailInfo/emailinfo.js
+++ portfolio/src/containers/emailInfo/emailinfo.js
@@ -1,9 +1,15 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import './emailinfo.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const EmailInfo = () => {
   const form = useRef();
+
+  const test = () => {
+    toast("test!!!");
+  }
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -25,7 +31,8 @@ const EmailInfo = () => {
   };
 
   return (
-    <form className='emailform' ref={form} onSubmit={sendEmail}>
+    <div>
+    <form className='emailform' ref={form} onSubmit={test}>
         <div className='emailform-header'>
             <div className='emailform-header-name'>
                 <input type="text" name="user_name" placeholder='Name'/>
@@ -41,6 +48,8 @@ const EmailInfo = () => {
             <input type="submit" value="Send" />
         </div>
     </form>
+<ToastContainer/>
+    </div>
   );
 };
 
